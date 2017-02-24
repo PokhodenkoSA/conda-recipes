dde7746b37488e1cb259851265eaf8c6d76c69d2
Mathew Topper <damm_horse@yahoo.co.uk>
2017-02-24 13:48:43 +0100

    Fix attrdict recipe

--- attrdict/recipe/bld.bat
+++ attrdict/recipe/bld.bat
@@ -1,4 +1,4 @@
-python setup.py install
+python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
