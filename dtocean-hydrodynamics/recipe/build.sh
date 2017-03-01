64a1adcf393a4c48d28c8ef980dfb1242b77df67
H0R5E <damm_horse@yahoo.co.uk>
2017-03-01 19:56:28 +0000

    Add dtocean-hydrodynamics recipe

---
+++ dtocean-hydrodynamics/recipe/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$PYTHON setup.py bootstrap || exit 1
+$PYTHON setup.py install || exit 1
+
+# Add more build steps here, if they are necessary.
+
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
